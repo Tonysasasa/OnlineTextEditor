5dfaf51eb08eeaf34e8d03f3f49a2ce28b50827d
Tony_Xia <Tony_Xia@Dell.com>
2024-09-19 19:52:17 +0800

    Add project files.

---
+++ TextEditor/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TextEditor.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
